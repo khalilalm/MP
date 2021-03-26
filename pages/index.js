4ed9285dce9074b26767e44198341ab835c55775
Khalil Al-Mujeeb <khalilalm@users.noreply.github.com>
2021-03-26 19:10:33 -0400

    make better:

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Head from "next/head";
-
 import styles from '../styles/Home.module.css'
-import { useSession } from 'next-auth/client'
+import Head from "next/head";
+import {signIn,signOut, useSession } from 'next-auth/client'
+import Link from "next/link";
 export default function Home() {
 
   const [session, loading] = useSession();
@@ -13,10 +13,13 @@ export default function Home() {
       <title>Minority Programmer Signup</title>
       <link rel="icon" href="/mp_gradient_rock.svg" />
     </Head>
-
-    <div className="signup-container">
+<main className={styles.main}>
+ {!session &&  (
+   <>
+   Not signed in <br />
+   <div className="signup-container">
       <form className="signup-form">
-        <h1>Welcome!</h1>
+        <h1 style={{"color":"white"}}>Welcome!</h1>
         <input
           required
           className="form-control"
@@ -33,25 +36,55 @@ export default function Home() {
 
         <input className="form-control" placeholder="Youre Password"></input>
         <br />
-        <div className={styles.user}>
-          {loading && <div className={styles.title}>Loading...</div>}
-          {session && <> <p style={{ marginBottom: '10px' }}> Welcome, {session.user.name ?? session.user.email}</p> <br />
-            <img src={session.user.image} alt="" className={styles.avatar} />
-          </>}
-          {!session &&
-            <>
-              <p className={styles.title}>Please Sign in</p>
-              {/* <img src="https://cdn.dribbble.com/users/759083/screenshots/6915953/2.gif" alt="" className={styles.avatar} /> */}        
-</>
-          }
+       
+          <div className="form-buttons">
+             <button className="button" onClick>Create Account</button>
+          <button className="button" onClick={signIn}>Sign In</button>
+          </div>
+        
+      </form>
+      
+    </div>
+   
+   </>
+ )}
+ {
+   session && (
+     <>
+     Signed in as {session.user.email} <br />
+     <div> You can now access page</div>
+     <button onClick={signOut}>sign out</button>
+     </>
+   )
+ }
+    <div className="signup-container">
+      <form className="signup-form">
+        <h1 style={{"color":"white"}}>Welcome!</h1>
+        <input
+          required
+          className="form-control"
+          placeholder="Youre Name"
+        ></input>
+        <br />
+
+        <input
+          required
+          className="form-control"
+          placeholder="Youre Email"
+        ></input>
+        <br />
+
+        <input className="form-control" placeholder="Youre Password"></input>
+        <br />
+       
           <div className="form-buttons">
              <button className="button" onClick>Create Account</button>
           <button className="button">Sign In</button>
           </div>
-         
-        </div>
+        
       </form>
       
     </div>
+    </main>
   </div>
 )}
